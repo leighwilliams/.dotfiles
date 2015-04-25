2aa2b8c7fda73f4c0e4828328401840a6fb911cf
Leigh Williams <leighwilliams@fastmail.fm>
2015-04-25 10:09:16 +1000

    Add .nvim to install script

--- install.sh
+++ install.sh
@@ -6,9 +6,9 @@
 
 ########## Variables
 
-dir=~/.dotfiles                                         # dotfiles directory
-olddir=~/.dotfiles_old                                  # old dotfiles backup directory
-files="antigen gitconfig nvimrc vim vimrc zshrc"    # list of files/folders to symlink in homedir
+dir=~/.dotfiles                                          # dotfiles directory
+olddir=~/.dotfiles_old                                   # old dotfiles backup directory
+files="antigen gitconfig nvim nvimrc vim vimrc zshrc"    # list of files/folders to symlink in homedir
 
 ##########
 
